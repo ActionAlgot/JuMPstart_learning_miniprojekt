056b2e44a0651ab28dbe6c2679a6509648218c39
JoelBerglund <joel.berglund94@gmail.com>
2016-07-22 12:29:03 +0200

    Stupid VS-git bullshit not tracking all files.

---
+++ Las-OchSkrivhjalp/DataAccess/QuestionsContext.cs
@@ -0,0 +1,16 @@
+﻿using Las_OchSkrivhjalp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Las_OchSkrivhjalp.DataAccess {
+	public class QuestionsContext : DbContext {
+		public QuestionsContext() : base("DefaultConnection") { }
+
+		public DbSet<QuestionCategory> Categories { get; set; }
+		//public DbSet<IQuestion> Questions { get; set; }
+		public DbSet<Highscore> ScoreBoard { get; set; }
+	}
+}
